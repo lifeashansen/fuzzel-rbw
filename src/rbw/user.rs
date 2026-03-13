e8b027f209c30fd5bfc2b21c05413585d32b70da
Andrew Fontaine <andrew@afontaine.ca>
2026-03-12 23:11:12 -0400

    feat: add copy & type for totp
    `rbw totp` returns 2fa codes for entries. Add configuration and code to
    fetch the totp value for a given entry and copy or type it out depending
    on the exit code.

--- src/rbw/user.rs
+++ src/rbw/user.rs
@@ -38,6 +38,8 @@ pub fn get_user(name_choice: String, name_to_users: HashMap<String, Vec<String>>
 
         let password: String = rbw::get::password(&name_choice, &user);
 
+        let totp: String = rbw::totp::totp(&name_choice, &user);
+
         // Copy user only
         if exit_code == cfg.copy_user_exit_code {
             let (result, _exit_code) = command::with_std_in_no_args("wl-copy", user.clone());
@@ -64,12 +66,29 @@ pub fn get_user(name_choice: String, name_to_users: HashMap<String, Vec<String>>
             }
 
             process::exit(0);
+        } else if exit_code == cfg.copy_totp_exit_code {
+            // Copy password only
+            let (result, _exit_code) = command::with_std_in_no_args("wl-copy", totp.clone());
+            let _result = command::no_std_in("cliphist", vec![String::from("delete-query"), password]);
+
+            match result {
+                Ok(_) => {
+                    let _ = notify::send(FRBW_ICON_NAME, FRBW_NAME, String::from("TOTP copied"));
+                }
+                Err(e) => return Err(e),
+            }
+
+            process::exit(0);
         } else if exit_code == cfg.type_user_exit_code {
             command::no_std_in("wtype", vec![user.clone()])?;
 
             process::exit(0);
         } else if exit_code == cfg.type_password_exit_code {
             command::no_std_in("wtype", vec![password.clone()])?;
+
+            process::exit(0);
+        } else if exit_code == cfg.type_totp_exit_code {
+            command::no_std_in("wtype", vec![totp.clone()])?;
 
             process::exit(0);
         }
